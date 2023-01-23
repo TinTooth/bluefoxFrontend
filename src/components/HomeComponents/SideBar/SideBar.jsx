0d42392d23b3f4c26508ab5e62f7e7bf24febc32
Ben O'Connell <boconne4@gmail.com>
2023-01-23 09:26:06 -0600

    Added Icons to Sidebar

--- src/components/HomeComponents/SideBar/SideBar.jsx
+++ src/components/HomeComponents/SideBar/SideBar.jsx
@@ -1,6 +1,11 @@
 import "./SideBar.css"
 import { useNavigate } from "react-router-dom";
 
+import bag from "../../../Images/bag.png"
+import cake from "../../../Images/cake.png"
+import cupcake from "../../../Images/cupcake.png"
+
+
 const SideBar = () => {
     const navigate = useNavigate()
 
@@ -9,12 +14,17 @@ const SideBar = () => {
         
     }
 
-
     return (
     <div className="sidebar">
-        <button className = 'sidebar-button'onClick={handleOrderClick}>Order</button>
-        <button className = 'sidebar-button'>Cake Menu</button>
-        <button className = 'sidebar-button'>Cupcake Menu</button>
+        <button className = 'sidebar-button'onClick={handleOrderClick}>
+            <img src= {bag} />
+        </button>
+        <button className = 'sidebar-button'>
+            <img src= {cake} />
+        </button>
+        <button className = 'sidebar-button'>
+            <img src= {cupcake} />
+        </button>
         <button className = 'sidebar-button'>Instagram</button>
     </div>
     )
